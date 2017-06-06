6d29994961db1c317edba6e5f7ada219110c2013
Rui Fonseca <ruimcf95@gmail.com>
2017-06-06 17:07:43 +0100

    Add code comments

--- trie.py
+++ trie.py
@@ -1,3 +1,11 @@
+"""
+Class node:
+    @chr - The string that identifies the node
+
+This is the base structure for a node in the Tree.
+Attribute 'endOfWord' indicates if this node represents the end of a word.
+Atribute children contains all the children nodes
+"""
 class node:
     def __init__(self, chr):
         self.char = chr
@@ -5,6 +13,14 @@ class node:
         self.endOfWord = False
 
 
+"""
+Function find:
+    @node - the starting node for the search
+    @pattern - the pattern to be found in the tree.
+
+For a pattern to be found, it must match with a sequence of nodes
+in the tree, and the final node's @endOfWord must True
+"""
 def find(node, pattern):
     for letter in pattern:
         tamanho = len(node.children)
@@ -29,11 +45,29 @@ def find(node, pattern):
     print("Found ", pattern)
 
 
+"""
+Function insertText:
+    @cur_node: the first node of the tree
+    @text: the text to be inserted in the tree
+
+If the text consists of multiple words seperated by spaces,
+each word will be inserted separately.
+This calls insert function which inserts words as in a classic prefix tree.
+"""
 def insertText(cur_node, text):
     tokens = text.split(" ")
     for token in tokens:
-        insert(cur_node, token)
+        insert(cur_node, token.rstrip())
 
+
+"""
+Function insert:
+    @cur_node: the first node of the tree
+    @word: the word to be inserted
+
+This function inserts the word in the tree as in a classic
+prefix tree fashion, that is: only one character per node
+"""
 def insert(cur_node, word):
     k = 0
     for letter in word:
@@ -63,9 +97,29 @@ def insert(cur_node, word):
                         print("Inserted letter ",letter)
     print("Inserted word", word)
 
-def remove(cur_node, word):
-    removeRecursive(cur_node, word, 0)
 
+"""
+Function remove:
+    @root_node: the first node of the tree
+    @word: the word to be removed from the tree
+
+This function only works for prefix trees that contain
+one character in each node.
+This function starts the recursive function removeRecursive.
+"""
+def remove(root_node, word):
+    removeRecursive(root_node, word, 0)
+
+
+"""
+Function removeRecursive:
+    @cur_node: the current node in the tree
+    @word: the word to be removed
+    @i: represents how far we have travelled down
+
+The function reaches the node that contains the end of the word
+and then recursively deletes the necessary nodes until the top.
+"""
 def removeRecursive(cur_node, word, i):
     if i == len(word):
         if(len(cur_node.children) == 0):
@@ -86,24 +140,51 @@ def removeRecursive(cur_node, word, i):
     return 0
 
 
+"""
+Function printTree:
+    @root_node: the first node of the tree
+
+This function will call printRecursive which prints all
+the words in the tree in a depth first search order.
+"""
 def printTree(root_node):
     print("####TREE####")
-    stack = []
+    queue = []
     for child in root_node.children:
-        printRecursive(child, stack)
+        printRecursive(child, queue)
 
-def printRecursive(cur_node, stack):
-    stack.append(cur_node.char)
+
+"""
+Function printRecursive:
+    @cur_node: the current node in the tree
+    @queue: the current queue of nodes
+
+The use of a queue allows to keep track of the
+path - that is, the content of the word discovered so far.
+
+print("".join(queue)) prints the content of the queue together.
+"""
+def printRecursive(cur_node, queue):
+    queue.append(cur_node.char)
     if(len(cur_node.children) == 0):
-        print("".join(stack))
-        stack.pop()
+        print("".join(queue))
+        queue.pop()
         return
     if(cur_node.endOfWord == True):
-        print("".join(stack))
+        print("".join(queue))
     for child in cur_node.children:
-        printRecursive(child, stack)
-    stack.pop()
+        printRecursive(child, queue)
+    queue.pop()
 
+
+
+"""
+Function printTreeNodes:
+    @cur_node
+
+This function prints the string of each node
+in the tree, in a depth first search style.
+"""
 def printTreeNodes(cur_node):
     print(cur_node.char)
     for child in cur_node.children:
@@ -111,10 +192,32 @@ def printTreeNodes(cur_node):
 
 
 
+"""
+Function insertRadix:
+    @root_node: the first node of the tree
+    @word: the word to be inserted
 
-def insertRadix(cur_node, word):
-    insertRadixRecursive(cur_node, word, 0)
+This function will call insertRadixRecursive which inserts
+the word in the tree in a radix tree style - that is, a node
+can contain more than one character
+"""
+def insertRadix(root_node, word):
+    insertRadixRecursive(root_node, word, 0)
 
+
+"""
+Function insertRadixRecursive:
+    @cur_node: the current node in the tree
+    @word: the word to be inserted
+    @letter: the current position in the word
+
+This function may transform existing tree nodes in order
+to insert a new word - splitting of a node.
+Example:
+    A node contains "Pratical" and we want to insert "Project"
+    This will result in the transformation the node to "Pr"
+    And nodes "atical" and "oject" to be added to its children.
+"""
 def insertRadixRecursive(cur_node, word, letter):
     tamanho = len(cur_node.children)
     if tamanho == 0:
@@ -166,6 +269,15 @@ def insertRadixRecursive(cur_node, word, letter):
     cur_node.children.append(new_node)
     return
 
+
+"""
+Function insertSuffixTree:
+    @root: the first node in the tree
+    @word: the word to be inserted
+
+This function inserts into a Radix Tree
+all the possible suffixes of the word.
+"""
 def insertSuffixTree(root, word):
     for i in range(len(word)):
         insertRadix(root, word[i:])
@@ -174,13 +286,25 @@ def insertSuffixTree(root, word):
 
 
 if __name__ == "__main__":
+    #Create a new tree - root node
     root = node('')
+
+    #Open input file - "inputText.txt" or "words.txt"
     fd = open("inputText.txt","r")
-    for line in range(5):
+
+    #The number of lines to be inserted
+    numberOfLines = 5
+
+    #Insert numberOfLines lines in the tree
+    #The method of insertion can be insertText | insertRadix | insertSuffixTree
+    #rstrip() removes end of line character
+    for line in range(numberOfLines):
         word = fd.readline().rstrip()
         if word != '':
             print("Inserting Word",word)
             insertSuffixTree(root, word)
+
+    #Print Tree
     printTreeNodes(root)
     printTree(root)
 
